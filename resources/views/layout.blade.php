70ac403bbbba7c9511074d4cc481e64bff614992
acsauk <acsauk@gmail.com>
2016-11-16 15:35:10 +0000

    installs dependencies and extracts styling out to style.css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/css/style.css" media="screen" title="no title">
 
   </head>
   <body>
